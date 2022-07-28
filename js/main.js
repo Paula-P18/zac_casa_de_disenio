4aeac6cc962a8fdeb3c7b9e9c00b9cd45eb1a8f7
Paula_P <paulabpina@gmail.com>
2022-07-27 22:46:07 -0300

    array

--- js/main.js
+++ js/main.js
@@ -12,23 +12,54 @@ function saludar() {
 saludar();
 function solicitarNombre() {
   let nombreIngresado = prompt("Ingrese su Nombre");
-  alert("El nombre ingresado es " + nombreIngresado);
+  alert("Hola " + nombreIngresado);
 }
 solicitarNombre();
-function solicitarProducto() {
-  let productoIngresado = prompt("Ingrese Producto");
-  alert("El Producto ingresado es " + productoIngresado);
+function solicitarMail() {
+  let mailIngresado = prompt("Ingrese su Mail");
+  alert("Su mail es " + mailIngresado);
 }
+solicitarMail();
+
+function solicitarProducto() {
+  let productoIngresado = prompt("Selecccione el producto que desea: Cuadro, Florero, Lampara, Jarron, Espejo");
+  alert("El Producto seleccionado es " + productoIngresado);
+}
+/* Productos con descuentos */
+
+class Producto{
+  constructor (nombre,precio) {
+    this.nombre = nombre.toUpperCase();
+    this.precio = parseFloat (precio);
+    this.vendido = false;
+    
+  }
+  descuento (){
+    this.precio = this.precio - (this.precio * 0,10);
+ }
+}
+const productos = [];
+productos.push(new Producto ("Cuadro", "3450"));
+productos.push(new Producto("Florero", "560"));
+productos.push(new Producto("Lampara", "900"));
+productos.push(new Producto("Jarron", "2800"));
+productos.push(new Producto("Espejo", "1500"));
+console.log(productos);
+
+
+for (const Producto of productos) {
+  Producto.descuento();
+  console.log( " " + Producto.nombre + " : $" + Producto.precio);
+alert( " " + Producto.nombre + " : $" + Producto.precio);
+ }
+  
+
 solicitarProducto();
-const suma = (a, b) => {
-  return a + b;
-};
-let precioProducto = 450;
-alert("Su valor es $" + precioProducto);
-const iva = (x) => {
-  return x * 0.21;
-};
-alert("IVA: " + iva(precioProducto));
-let nuevoPrecio = suma(precioProducto, iva(precioProducto));
-console.log("El valor del Producto es" + nuevoPrecio);
-alert(" El valor del producto total es " + nuevoPrecio);
+ alert("Solo acepta pago por debito, al ser producto seleccionado")
+
+
+
+
+
+
+
